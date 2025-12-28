096293ac3978c913c224242f3cae312c475db297
Ana E. San <67754798+eusanchez@users.noreply.github.com>
2025-12-28 05:33:20 +0000

    adding constraints to the main test

--- uvm_transaction.sv
+++ uvm_transaction.sv
@@ -5,7 +5,12 @@
     rand bit [7:0] a;
     rand bit [7:0] b;
          bit [8:0] y; // observed
-
+    
+    constraint values_def {
+      a inside {[1:20]};
+      b inside {[1:20]};
+    }
+    
     `uvm_object_utils_begin(add_txn)
       `uvm_field_int(a, UVM_ALL_ON)
       `uvm_field_int(b, UVM_ALL_ON)
